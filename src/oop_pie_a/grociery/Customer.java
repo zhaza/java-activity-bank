2f2a402b1fb9798c17602a9e8d13f53ee9138856
zhaza <Khail.ballin12@yahoo.com>
2022-06-24 14:12:23 -0700

    Resolve merge conflicts

--- src/oop_pie_a/grociery/Customer.java
+++ src/oop_pie_a/grociery/Customer.java
@@ -1,4 +1,30 @@
 package oop_pie_a.grociery;
 
+import java.util.ArrayList;
+
 public class Customer {
+
+    private String name;
+    public ArrayList<String> cart;
+    private int age;
+    private boolean hasKids;
+    private boolean hasPets;
+
+    public String askManagersName() {
+        System.out.println(managerName);
+    }
+    private String askManagersSalary() {
+        System.out.println(managerSalary);
+    }
+    public String addItemToCart(item) {
+        cart.add(item);
+    }
+    public String checkedoutAtRegister() {
+        System.out.println("Completed purchase!");
+    }
+    public static void main(String[] args) {
+
+    }
+
+
 }
--- src/oop_pie_a/grociery/GroceryStore.java
+++ src/oop_pie_a/grociery/GroceryStore.java
@@ -2,8 +2,20 @@ package oop_pie_a.grociery;
 
 public class GroceryStore {
 
-
-    public static void main(String[] args) {
+    public String storeName;
+    public String managerName;
+    private int managerSalary;
+    public int[] stock;
+    public String greetCustomer() {
+        System.out.println("Welcome to "+storeName+"!");
+    }
+    public int makeTransaction(int[] stock, ) {
 
     }
+
+    public static void main(String[] args) {
+        storeName = "Safeway";
+
+    }
+
 }
